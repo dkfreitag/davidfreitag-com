7d124e3d4077871862a675e04abf9ab913a42b36
dkfreitag <dkfreitag@gmail.com>
2021-12-19 14:37:20 -0500

    Rebuilding site Sun Dec 19 14:37:20 EST 2021

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf public
-git rm -r --cached public
+git rm -r --cached public --force
 git submodule add https://github.com/dkfreitag/dkfreitag.github.io.git public
 
 ./build_site.sh
