a4e0ce8a86ee295aef9f37278dbe9f9b22a02ac8
dkfreitag <dkfreitag@gmail.com>
2021-12-19 14:55:08 -0500

    Updates committed on Sun Dec 19 14:55:08 EST 2021

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 git rm -r --cached public
 rm -rf public
-git submodule add https://github.com/dkfreitag/dkfreitag.github.io.git public
+git submodule add --force https://github.com/dkfreitag/dkfreitag.github.io.git public
 
 ./build_site.sh
 
